d40b80d33ea0a9f3d2e6124caea50bc342da2b77
Tjari <zadigsm@outlook.com>
2026-05-23 17:53:27 +0700

    a05m: init
    Partially based on Fede2782's a34x target, but adapted for a05m
    
    Co-authored-by: Fede2782 <78815152+Fede2782@users.noreply.github.com>
    Co-authored-by: Smiley <59495159+smiley9000@users.noreply.github.com>

---
+++ target/a05m/patches/stock_props/customize.sh
@@ -0,0 +1,1 @@
+SET_PROP "system_ext" "ro.audio.ihaladaptervendorextension_enabled" "false"
---
+++ target/a05m/sff.sh
@@ -0,0 +1,10 @@
+# Copyright (c) 2026 Salvo Giangreco
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# SEC Floating Feature configuration file for Galaxy A05
+
+# Enable extra brightness feature
+SEC_FLOATING_FEATURE_LCD_SUPPORT_EXTRA_BRIGHTNESS=TRUE
+
+# Always show remaster button in Gallery
+SEC_FLOATING_FEATURE_SAIV_CONFIG_AI_REVITAL_VERSION=1.7,1
