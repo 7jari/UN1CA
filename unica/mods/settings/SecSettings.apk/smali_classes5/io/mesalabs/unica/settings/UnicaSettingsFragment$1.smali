matei9/UN1CA_SM7125

unica: mods: Introduce UN1CA Settings.
* ALL the credits go to Salvo, not my settings.

unica/mods/settings/SecSettings.apk/smali_classes5/io/mesalabs/unica/settings/UnicaSettingsFragment$1.smali
@@ -1,0 +1,80 @@
+.class public final Lio/mesalabs/unica/settings/UnicaSettingsFragment$1;
+.super Ljava/lang/Object;
+.source "UnicaSettingsFragment.java"
+
+# interfaces
+.implements Landroidx/appcompat/widget/PopupMenu$OnMenuItemClickListener;
+
+
+# instance fields
+.field public final synthetic this$0:Lio/mesalabs/unica/settings/UnicaSettingsFragment;
+
+
+# direct methods
+.method public constructor <init>(Lio/mesalabs/unica/settings/UnicaSettingsFragment;)V
+    .locals 0
+
+    invoke-direct {p0}, Ljava/lang/Object;-><init>()V
+
+    iput-object p1, p0, Lio/mesalabs/unica/settings/UnicaSettingsFragment$1;->this$0:Lio/mesalabs/unica/settings/UnicaSettingsFragment;
+
+    return-void
+.end method
+
+
+# virtual methods
+.method public final onMenuItemClick(Landroid/view/MenuItem;)V
+    .locals 2
+
+    iget-object p0, p0, Lio/mesalabs/unica/settings/UnicaSettingsFragment$1;->this$0:Lio/mesalabs/unica/settings/UnicaSettingsFragment;
+
+    invoke-virtual {p0}, Landroidx/fragment/app/Fragment;->getContext()Landroid/content/Context;
+
+    move-result-object p0
+
+    const-string v0, "power"
+
+    invoke-virtual {p0, v0}, Landroid/content/Context;->getSystemService(Ljava/lang/String;)Ljava/lang/Object;
+
+    move-result-object p0
+
+    check-cast p0, Landroid/os/PowerManager;
+
+    invoke-interface {p1}, Landroid/view/MenuItem;->getItemId()I
+
+    move-result p1
+
+    const/4 v0, 0x1
+
+    if-eq p1, v0, :cond_2
+
+    const/4 v1, 0x2
+
+    if-eq p1, v1, :cond_1
+
+    const/4 v1, 0x3
+
+    if-eq p1, v1, :cond_0
+
+    const/4 p1, 0x0
+
+    goto :goto_0
+
+    :cond_0
+    const-string p1, "download"
+
+    goto :goto_0
+
+    :cond_1
+    const-string p1, "fastboot"
+
+    goto :goto_0
+
+    :cond_2
+    const-string p1, "recovery"
+
+    :goto_0
+    invoke-virtual {p0, p1}, Landroid/os/PowerManager;->reboot(Ljava/lang/String;)V
+
+    return-void
+.end method

unica/mods/settings/SecSettings.apk/smali_classes5/io/mesalabs/unica/utils/Utils.smali
@@ -1,0 +1,198 @@
+.class public final Lio/mesalabs/unica/utils/Utils;
+.super Ljava/lang/Object;
+.source "Utils.java"
+
+
+# static fields
+.field private static final ACTION_MY_FILES_PICK_DATA:Ljava/lang/String; = "com.sec.android.app.myfiles.PICK_DATA"
+
+.field private static final EXTRA_MY_FILES_CONTENT_EXTENSION:Ljava/lang/String; = "CONTENT_EXTENSION"
+
+.field private static final EXTRA_MY_FILES_NEED_SCAN:Ljava/lang/String; = "needScan"
+
+.field private static final MY_FILES_PACKAGE_NAME:Ljava/lang/String; = "com.sec.android.app.myfiles"
+
+
+# direct methods
+.method private constructor <init>()V
+    .locals 0
+
+    invoke-direct {p0}, Ljava/lang/Object;-><init>()V
+
+    return-void
+.end method
+
+.method public static createRebootDialog(Landroid/content/Context;)Landroidx/appcompat/app/AlertDialog;
+    .locals 4
+
+    new-instance v0, Landroidx/appcompat/app/AlertDialog$Builder;
+
+    invoke-direct {v0, p0}, Landroidx/appcompat/app/AlertDialog$Builder;-><init>(Landroid/content/Context;)V
+
+    const-string v1, "unica_dialog_reboot_title"
+
+    const-string v2, "string"
+
+    invoke-static {v2, v1}, Lio/mesalabs/unica/utils/Utils;->getResourceId(Ljava/lang/String;Ljava/lang/String;)I
+
+    move-result v1
+
+    invoke-virtual {v0, v1}, Landroidx/appcompat/app/AlertDialog$Builder;->setTitle(I)Landroidx/appcompat/app/AlertDialog$Builder;
+
+    const-string v1, "unica_dialog_reboot_msg"
+
+    invoke-static {v2, v1}, Lio/mesalabs/unica/utils/Utils;->getResourceId(Ljava/lang/String;Ljava/lang/String;)I
+
+    move-result v1
+
+    invoke-virtual {v0, v1}, Landroidx/appcompat/app/AlertDialog$Builder;->setMessage(I)Landroidx/appcompat/app/AlertDialog$Builder;
+
+    const-string v1, "unica_dialog_reboot_positive"
+
+    invoke-static {v2, v1}, Lio/mesalabs/unica/utils/Utils;->getResourceId(Ljava/lang/String;Ljava/lang/String;)I
+
+    move-result v1
+
+    new-instance v3, Lio/mesalabs/unica/utils/Utils$$ExternalSyntheticLambda0;
+
+    invoke-direct {v3, p0}, Lio/mesalabs/unica/utils/Utils$$ExternalSyntheticLambda0;-><init>(Landroid/content/Context;)V
+
+    invoke-virtual {v0, v1, v3}, Landroidx/appcompat/app/AlertDialog$Builder;->setPositiveButton(ILandroid/content/DialogInterface$OnClickListener;)Landroidx/appcompat/app/AlertDialog$Builder;
+
+    const-string p0, "unica_dialog_reboot_negative"
+
+    invoke-static {v2, p0}, Lio/mesalabs/unica/utils/Utils;->getResourceId(Ljava/lang/String;Ljava/lang/String;)I
+
+    move-result p0
+
+    const/4 v1, 0x0
+
+    invoke-virtual {v0, p0, v1}, Landroidx/appcompat/app/AlertDialog$Builder;->setNegativeButton(ILandroid/content/DialogInterface$OnClickListener;)Landroidx/appcompat/app/AlertDialog$Builder;
+
+    invoke-virtual {v0}, Landroidx/appcompat/app/AlertDialog$Builder;->create()Landroidx/appcompat/app/AlertDialog;
+
+    move-result-object p0
+
+    return-object p0
+.end method
+
+.method public static getFilePickerOpenIntent(Landroid/content/Context;[Ljava/lang/String;[Ljava/lang/String;)Landroid/content/Intent;
+    .locals 2
+
+    const-string v0, "com.sec.android.app.myfiles"
+
+    invoke-static {p0, v0}, Lcom/samsung/android/settings/PkgUtils;->isPackageEnabled(Landroid/content/Context;Ljava/lang/String;)Z
+
+    move-result p0
+
+    if-eqz p0, :cond_0
+
+    new-instance p0, Landroid/content/Intent;
+
+    const-string v0, "com.sec.android.app.myfiles.PICK_DATA"
+
+    invoke-direct {p0, v0}, Landroid/content/Intent;-><init>(Ljava/lang/String;)V
+
+    const-string v0, "needScan"
+
+    const/4 v1, 0x1
+
+    invoke-virtual {p0, v0, v1}, Landroid/content/Intent;->putExtra(Ljava/lang/String;Z)Landroid/content/Intent;
+
+    move-result-object p0
+
+    if-eqz p2, :cond_1
+
+    const-string v0, ";"
+
+    invoke-static {v0, p2}, Landroid/text/TextUtils;->join(Ljava/lang/CharSequence;[Ljava/lang/Object;)Ljava/lang/String;
+
+    move-result-object p2
+
+    const-string v0, "CONTENT_EXTENSION"
+
+    invoke-virtual {p0, v0, p2}, Landroid/content/Intent;->putExtra(Ljava/lang/String;Ljava/lang/String;)Landroid/content/Intent;
+
+    goto :goto_0
+
+    :cond_0
+    new-instance p0, Landroid/content/Intent;
+
+    const-string p2, "android.intent.action.OPEN_DOCUMENT"
+
+    invoke-direct {p0, p2}, Landroid/content/Intent;-><init>(Ljava/lang/String;)V
+
+    const-string p2, "android.intent.category.OPENABLE"
+
+    invoke-virtual {p0, p2}, Landroid/content/Intent;->addCategory(Ljava/lang/String;)Landroid/content/Intent;
+
+    move-result-object p0
+
+    const-string p2, "*/*"
+
+    invoke-virtual {p0, p2}, Landroid/content/Intent;->setType(Ljava/lang/String;)Landroid/content/Intent;
+
+    move-result-object p0
+
+    :cond_1
+    :goto_0
+    if-eqz p1, :cond_2
+
+    const-string p2, "android.intent.extra.MIME_TYPES"
+
+    invoke-virtual {p0, p2, p1}, Landroid/content/Intent;->putExtra(Ljava/lang/String;[Ljava/lang/String;)Landroid/content/Intent;
+
+    :cond_2
+    return-object p0
+.end method
+
+.method public static getResourceId(Ljava/lang/String;Ljava/lang/String;)I
+    .locals 3
+
+    sget-object v0, Lcom/android/settings/overlay/FeatureFactoryImpl;->_appContext:Lcom/android/settings/SettingsApplication;
+
+    invoke-virtual {v0}, Landroid/content/Context;->getResources()Landroid/content/res/Resources;
+
+    move-result-object v0
+
+    const-string v1, "com.android.settings"
+
+    invoke-virtual {v0, p1, p0, v1}, Landroid/content/res/Resources;->getIdentifier(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)I
+
+    move-result v0
+
+    if-eqz v0, :cond_0
+
+    return v0
+
+    :cond_0
+    new-instance v0, Landroid/content/res/Resources$NotFoundException;
+
+    new-instance v1, Ljava/lang/StringBuilder;
+
+    const-string v2, "Cannot find resource ID for @"
+
+    invoke-direct {v1, v2}, Ljava/lang/StringBuilder;-><init>(Ljava/lang/String;)V
+
+    invoke-virtual {v1, p0}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder;
+
+    move-result-object p0
+
+    const-string v1, "/"
+
+    invoke-virtual {p0, v1}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder;
+
+    move-result-object p0
+
+    invoke-virtual {p0, p1}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder;
+
+    move-result-object p0
+
+    invoke-virtual {p0}, Ljava/lang/StringBuilder;->toString()Ljava/lang/String;
+
+    move-result-object p0
+
+    invoke-direct {v0, p0}, Landroid/content/res/Resources$NotFoundException;-><init>(Ljava/lang/String;)V
+
+    throw v0
+.end method

7jari/UN1CA

unica/mods/settings/SecSettings.apk/smali_classes5/io/mesalabs/unica/settings/UnicaSettingsFragment$1.smali
@@ -1,0 +1,82 @@
+.class public final Lio/mesalabs/unica/settings/UnicaSettingsFragment$1;
+.super Ljava/lang/Object;
+.source "UnicaSettingsFragment.java"
+
+# interfaces
+.implements Landroidx/appcompat/widget/PopupMenu$OnMenuItemClickListener;
+
+
+# instance fields
+.field public final synthetic this$0:Lio/mesalabs/unica/settings/UnicaSettingsFragment;
+
+
+# direct methods
+.method public constructor <init>(Lio/mesalabs/unica/settings/UnicaSettingsFragment;)V
+    .locals 0
+
+    invoke-direct {p0}, Ljava/lang/Object;-><init>()V
+
+    iput-object p1, p0, Lio/mesalabs/unica/settings/UnicaSettingsFragment$1;->this$0:Lio/mesalabs/unica/settings/UnicaSettingsFragment;
+
+    return-void
+.end method
+
+
+# virtual methods
+.method public final onMenuItemClick(Landroid/view/MenuItem;)Z
+    .locals 2
+
+    iget-object p0, p0, Lio/mesalabs/unica/settings/UnicaSettingsFragment$1;->this$0:Lio/mesalabs/unica/settings/UnicaSettingsFragment;
+
+    invoke-virtual {p0}, Landroidx/fragment/app/Fragment;->getContext()Landroid/content/Context;
+
+    move-result-object p0
+
+    const-string v0, "power"
+
+    invoke-virtual {p0, v0}, Landroid/content/Context;->getSystemService(Ljava/lang/String;)Ljava/lang/Object;
+
+    move-result-object p0
+
+    check-cast p0, Landroid/os/PowerManager;
+
+    invoke-interface {p1}, Landroid/view/MenuItem;->getItemId()I
+
+    move-result p1
+
+    const/4 v0, 0x1
+
+    if-eq p1, v0, :cond_2
+
+    const/4 v1, 0x2
+
+    if-eq p1, v1, :cond_1
+
+    const/4 v1, 0x3
+
+    if-eq p1, v1, :cond_0
+
+    const/4 p1, 0x0
+
+    goto :goto_0
+
+    :cond_0
+    const-string p1, "download"
+
+    goto :goto_0
+
+    :cond_1
+    const-string p1, "fastboot"
+
+    goto :goto_0
+
+    :cond_2
+    const-string p1, "recovery"
+
+    :goto_0
+    invoke-virtual {p0, p1}, Landroid/os/PowerManager;->reboot(Ljava/lang/String;)V
+
+    const/4 p0, 0x1
+
+    return p0
+.end method

unica/mods/settings/SecSettings.apk/smali_classes5/io/mesalabs/unica/utils/Utils.smali
@@ -1,0 +1,198 @@
+.class public final Lio/mesalabs/unica/utils/Utils;
+.super Ljava/lang/Object;
+.source "Utils.java"
+
+
+# static fields
+.field private static final ACTION_MY_FILES_PICK_DATA:Ljava/lang/String; = "com.sec.android.app.myfiles.PICK_DATA"
+
+.field private static final EXTRA_MY_FILES_CONTENT_EXTENSION:Ljava/lang/String; = "CONTENT_EXTENSION"
+
+.field private static final EXTRA_MY_FILES_NEED_SCAN:Ljava/lang/String; = "needScan"
+
+.field private static final MY_FILES_PACKAGE_NAME:Ljava/lang/String; = "com.sec.android.app.myfiles"
+
+
+# direct methods
+.method private constructor <init>()V
+    .locals 0
+
+    invoke-direct {p0}, Ljava/lang/Object;-><init>()V
+
+    return-void
+.end method
+
+.method public static createRebootDialog(Landroid/content/Context;)Landroidx/appcompat/app/AlertDialog;
+    .locals 5
+
+    new-instance v0, Landroidx/appcompat/app/AlertDialog$Builder;
+
+    invoke-direct {v0, p0}, Landroidx/appcompat/app/AlertDialog$Builder;-><init>(Landroid/content/Context;)V
+
+    const-string v1, "unica_dialog_reboot_title"
+
+    const-string v2, "string"
+
+    invoke-static {v2, v1}, Lio/mesalabs/unica/utils/Utils;->getResourceId(Ljava/lang/String;Ljava/lang/String;)I
+
+    move-result v1
+
+    invoke-virtual {v0, v1}, Landroidx/appcompat/app/AlertDialog$Builder;->setTitle(I)V
+
+    const-string v1, "unica_dialog_reboot_msg"
+
+    invoke-static {v2, v1}, Lio/mesalabs/unica/utils/Utils;->getResourceId(Ljava/lang/String;Ljava/lang/String;)I
+
+    move-result v1
+
+    invoke-virtual {v0, v1}, Landroidx/appcompat/app/AlertDialog$Builder;->setMessage(I)V
+
+    const-string v1, "unica_dialog_reboot_positive"
+
+    invoke-static {v2, v1}, Lio/mesalabs/unica/utils/Utils;->getResourceId(Ljava/lang/String;Ljava/lang/String;)I
+
+    move-result v1
+
+    new-instance v3, Lio/mesalabs/unica/utils/Utils$$ExternalSyntheticLambda0;
+
+    invoke-direct {v3, p0}, Lio/mesalabs/unica/utils/Utils$$ExternalSyntheticLambda0;-><init>(Landroid/content/Context;)V
+
+    invoke-virtual {v0, v1, v3}, Landroidx/appcompat/app/AlertDialog$Builder;->setPositiveButton(ILandroid/content/DialogInterface$OnClickListener;)V
+
+    const-string v3, "unica_dialog_reboot_negative"
+
+    invoke-static {v2, v3}, Lio/mesalabs/unica/utils/Utils;->getResourceId(Ljava/lang/String;Ljava/lang/String;)I
+
+    move-result v3
+
+    const/4 v1, 0x0
+
+    invoke-virtual {v0, v3, v1}, Landroidx/appcompat/app/AlertDialog$Builder;->setNegativeButton(ILandroid/content/DialogInterface$OnClickListener;)V
+
+    invoke-virtual {v0}, Landroidx/appcompat/app/AlertDialog$Builder;->create()Landroidx/appcompat/app/AlertDialog;
+
+    move-result-object p0
+
+    return-object p0
+.end method
+
+.method public static getFilePickerOpenIntent(Landroid/content/Context;[Ljava/lang/String;[Ljava/lang/String;)Landroid/content/Intent;
+    .locals 2
+
+    const-string v0, "com.sec.android.app.myfiles"
+
+    invoke-static {p0, v0}, Lcom/samsung/android/settings/PkgUtils;->isPackageEnabled(Landroid/content/Context;Ljava/lang/String;)Z
+
+    move-result p0
+
+    if-eqz p0, :cond_0
+
+    new-instance p0, Landroid/content/Intent;
+
+    const-string v0, "com.sec.android.app.myfiles.PICK_DATA"
+
+    invoke-direct {p0, v0}, Landroid/content/Intent;-><init>(Ljava/lang/String;)V
+
+    const-string v0, "needScan"
+
+    const/4 v1, 0x1
+
+    invoke-virtual {p0, v0, v1}, Landroid/content/Intent;->putExtra(Ljava/lang/String;Z)Landroid/content/Intent;
+
+    move-result-object p0
+
+    if-eqz p2, :cond_1
+
+    const-string v0, ";"
+
+    invoke-static {v0, p2}, Landroid/text/TextUtils;->join(Ljava/lang/CharSequence;[Ljava/lang/Object;)Ljava/lang/String;
+
+    move-result-object p2
+
+    const-string v0, "CONTENT_EXTENSION"
+
+    invoke-virtual {p0, v0, p2}, Landroid/content/Intent;->putExtra(Ljava/lang/String;Ljava/lang/String;)Landroid/content/Intent;
+
+    goto :goto_0
+
+    :cond_0
+    new-instance p0, Landroid/content/Intent;
+
+    const-string p2, "android.intent.action.OPEN_DOCUMENT"
+
+    invoke-direct {p0, p2}, Landroid/content/Intent;-><init>(Ljava/lang/String;)V
+
+    const-string p2, "android.intent.category.OPENABLE"
+
+    invoke-virtual {p0, p2}, Landroid/content/Intent;->addCategory(Ljava/lang/String;)Landroid/content/Intent;
+
+    move-result-object p0
+
+    const-string p2, "*/*"
+
+    invoke-virtual {p0, p2}, Landroid/content/Intent;->setType(Ljava/lang/String;)Landroid/content/Intent;
+
+    move-result-object p0
+
+    :cond_1
+    :goto_0
+    if-eqz p1, :cond_2
+
+    const-string p2, "android.intent.extra.MIME_TYPES"
+
+    invoke-virtual {p0, p2, p1}, Landroid/content/Intent;->putExtra(Ljava/lang/String;[Ljava/lang/String;)Landroid/content/Intent;
+
+    :cond_2
+    return-object p0
+.end method
+
+.method public static getResourceId(Ljava/lang/String;Ljava/lang/String;)I
+    .locals 3
+
+    sget-object v0, Lcom/android/settings/overlay/FeatureFactoryImpl;->_appContext:Lcom/android/settings/SettingsApplication;
+
+    invoke-virtual {v0}, Landroid/content/Context;->getResources()Landroid/content/res/Resources;
+
+    move-result-object v0
+
+    const-string v1, "com.android.settings"
+
+    invoke-virtual {v0, p1, p0, v1}, Landroid/content/res/Resources;->getIdentifier(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)I
+
+    move-result v0
+
+    if-eqz v0, :cond_0
+
+    return v0
+
+    :cond_0
+    new-instance v0, Landroid/content/res/Resources$NotFoundException;
+
+    new-instance v1, Ljava/lang/StringBuilder;
+
+    const-string v2, "Cannot find resource ID for @"
+
+    invoke-direct {v1, v2}, Ljava/lang/StringBuilder;-><init>(Ljava/lang/String;)V
+
+    invoke-virtual {v1, p0}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder;
+
+    move-result-object p0
+
+    const-string v1, "/"
+
+    invoke-virtual {p0, v1}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder;
+
+    move-result-object p0
+
+    invoke-virtual {p0, p1}, Ljava/lang/StringBuilder;->append(Ljava/lang/String;)Ljava/lang/StringBuilder;
+
+    move-result-object p0
+
+    invoke-virtual {p0}, Ljava/lang/StringBuilder;->toString()Ljava/lang/String;
+
+    move-result-object p0
+
+    invoke-direct {v0, p0}, Landroid/content/res/Resources$NotFoundException;-><init>(Ljava/lang/String;)V
+
+    throw v0
+.end method